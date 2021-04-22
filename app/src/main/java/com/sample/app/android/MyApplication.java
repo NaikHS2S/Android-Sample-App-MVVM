158d89db3c16c51e0974701cdd1ccacc3cf83249
NaikHS2S <hknaik988@gmail.com>
2021-04-22 23:08:31 +0530

    updated lib

---
+++ app/src/main/java/com/sample/app/android/MyApplication.java
@@ -0,0 +1,9 @@
+package com.sample.app.android;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApplication extends Application {
+}

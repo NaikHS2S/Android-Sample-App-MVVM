8e863f061e7f44669ef845acbddee712a19bc484
NaikHS2S <hknaik988@gmail.com>
2021-05-13 13:15:04 +0530

    updated CI yml file

--- app/src/main/java/com/sample/app/android/data/model/LoggedInUser.kt
+++ app/src/main/java/com/sample/app/android/data/model/LoggedInUser.kt
@@ -1,6 +1,8 @@
 package com.sample.app.android.data.model
 
+import javax.inject.Inject
+
 /**
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
-data class LoggedInUser(val userId: String, val displayName: String)
+data class LoggedInUser @Inject constructor(val userId: String, val displayName: String)

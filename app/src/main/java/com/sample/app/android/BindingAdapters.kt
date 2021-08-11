e6f871f007771d0f3e545b3fa334b78c7af41176
NaikHS2S <NaikH988@Outlook.com>
2021-08-11 13:46:01 +0530

    Build update

--- app/src/main/java/com/sample/app/android/BindingAdapters.kt
+++ app/src/main/java/com/sample/app/android/BindingAdapters.kt
@@ -6,33 +6,33 @@ import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import com.sample.app.android.ui.custom.view.CustomTextInputEditText
 
-@BindingAdapter("app:userNameError")
+@BindingAdapter("userNameError")
 fun displayUserNameError(view: EditText, userError: Int?) {
     if(userError != null) {
         view.error = view.context.getString(R.string.invalid_username)
     }
 }
 
-@BindingAdapter("app:passError")
+@BindingAdapter("passError")
 fun displayPasswordError(view: EditText, passWordError: Int?) {
     if(passWordError != null) {
         view.error = view.context.getString(R.string.invalid_password)
     }
 }
 
-@BindingAdapter("app:textError")
+@BindingAdapter("textError")
 fun displayPasswordError(view: CustomTextInputEditText, error: String?) {
     if(error != null && error.isNotEmpty()) {
         view.error = error
     }
 }
 
-@BindingAdapter("app:loginEnable")
+@BindingAdapter("loginEnable")
 fun loginEnable(view: Button, isLoginEnable: Boolean) {
     view.isEnabled = isLoginEnable
 }
 
-@BindingAdapter(value = ["app:accessibilityReadText", "app:isClickable"], requireAll = false)
+@BindingAdapter(value = ["accessibilityReadText", "isClickable"], requireAll = false)
 fun updateValue(view: Button, accessibilityReadText: String, isClickable: Boolean) {
     view.importantForAccessibility = if(accessibilityReadText.isNotEmpty())  View.IMPORTANT_FOR_ACCESSIBILITY_YES else View.IMPORTANT_FOR_ACCESSIBILITY_NO
     view.contentDescription = if(accessibilityReadText.isNotEmpty()) accessibilityReadText else view.text
